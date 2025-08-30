2692e0e1b094504fca4e06c070ec913221c51cfb
Kydorpe <lopedro45@gmail.com>
2025-08-30 17:42:53 -0300

    Resolvido os exercicios da Aula001

---
+++ Au001/quest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// Quest 1
+
+//Console.Write("What your name? ");
+//string name = Console.ReadLine();
+//Console.Write("how old are you? ");
+//int age = int.Parse(Console.ReadLine());
+//Console.Write("Where you live? ");
+//string country = Console.ReadLine();
+
+//Console.WriteLine("");
+//Console.Write($"Hello, my name is {name}. ");
+//Console.WriteLine($"I'm {age} years old!");
+//Console.Write($"I live in {country}.");
+
+// Quest 2
+
+//const double PI = 3.1415;
+
+//Console.Write("Enter the radius of the circle: ");
+//double radius = double.Parse(Console.ReadLine());
+//double circleArea = PI * (radius * radius);
+
+//Console.WriteLine($"The circle area is: {circleArea:F2}");
+
+// Quest 3
+
+//Console.Write("Enter your weight (kg): ");
+//double weight = double.Parse(Console.ReadLine());
+//Console.Write("Enter your height (mt): ");
+//double height = double.Parse(Console.ReadLine());
+
+//double IMC = (height * height)/weight;
+
+//Console.WriteLine($"Your IMC is: {IMC}");
+
+// Quest 4
+
+//Console.Write("Enter first number: ");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.Write("Enter second number: ");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//Console.WriteLine($"The sum of the numbers: {firstNumber + secondNumber}");
+//Console.WriteLine($"The subtraction of the numbers: {firstNumber - secondNumber}");
+//Console.WriteLine($"The multiplication of the numbers: {firstNumber * secondNumber}");
+//Console.WriteLine($"The division of the numbers: {firstNumber / secondNumber}");
+
+// Quest 5
+
+//Console.WriteLine("Multiplication table:");
+//Console.Write("Enter the number: ");
+//int table = int.Parse(Console.ReadLine());
+
+//for (int i=0; i < 11; i++)
+//{
+//    Console.WriteLine($"// {table} times {i} = {table * i}");
+//}
