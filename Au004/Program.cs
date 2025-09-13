61cbb280136466d61e7a4d03df8a5d2ba0b6f258
Kydorpe <lopedro45@gmai.com>
2025-09-13 10:43:18 -0300

    update

--- Au004/Program.cs
+++ Au004/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Aula04
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string [,] gradeStudant = new string [5, 3];
+            for (int i = 0; i < gradeStudant[i, 0].Length; i++)
+            {
+                Console.Write("Insert Studant Name: ");
+                gradeStudant[i, 0] = Console.ReadLine();
+                Console.WriteLine("");
+                for (int j = 0; j < gradeStudant[i, j].Length; j++)
+                {
+                    Console.Write($"Insert {gradeStudant[i,0]} grade: ");
+                    gradeStudant[i,j] = Console.ReadLine();
+                }
+            }
+           
+        }
+    }
+}
