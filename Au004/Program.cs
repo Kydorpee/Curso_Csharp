672e74df5ba127b5710f4eff64971da4f0418662
Kydorpe <lopedro45@gmail.com>
2025-09-14 13:32:15 -0300

    update au004

--- Au004/Program.cs
+++ Au004/Program.cs
@@ -1,25 +1,63 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+
 
 namespace Aula04
 {
+
+    
+
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            string [,] gradeStudant = new string [5, 3];
-            for (int i = 0; i < gradeStudant[i, 0].Length; i++)
-            {
-                Console.Write("Insert Studant Name: ");
-                gradeStudant[i, 0] = Console.ReadLine();
-                Console.WriteLine("");
-                for (int j = 0; j < gradeStudant[i, j].Length; j++)
-                {
-                    Console.Write($"Insert {gradeStudant[i,0]} grade: ");
-                    gradeStudant[i,j] = Console.ReadLine();
-                }
-            }
-           
-        }
+        //#region Quest 1
+        //public static void Main(string[] args)
+        //{
+        //    string[,] gradeStudant = new string[5, 4];
+        //    double[] gradeTotal = new double[5];
+        //    double[] gradeAverage = new double[5];
+
+        //    void GetGrade()
+        //    {
+
+
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            Console.Write("Insert studant name : ");
+        //            gradeStudant[i, 0] = Console.ReadLine();
+
+        //            for (int j = 1; j <= 3; j++)
+        //            {
+        //                Console.Write($"Insert grade of {gradeStudant[i, 0]}: ");
+        //                gradeStudant[i, j] = Console.ReadLine();
+        //                gradeTotal[i] += double.Parse(gradeStudant[i, j]);
+
+
+        //            }
+
+        //        }
+
+        //    }
+        //    void GetAverage()
+        //    {
+
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            gradeAverage[i] = gradeTotal[i] / 3;
+        //            Console.WriteLine($"Average of {gradeStudant[i, 0]} is: {gradeAverage[i]: F2}");
+
+        //        }
+        //    }
+
+        //    GetGrade();
+        //    GetAverage();
+
+        //}
+        //#endregion
+
+
     }
+
+
+
+
+
 }
